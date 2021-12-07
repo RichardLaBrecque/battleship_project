9612c40b5cdd746cd268b45f4b9387e4d33f1052
Tyler Ross <tross020890@gmail.com>
2021-12-07 02:02:16 -0600

    Fix: moved misplaced end, add require lib/ship

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,4 +1,5 @@
 require './lib/cell'
+require './lib/ship'
 RSpec.describe Cell do
   it 'exists' do
     cell = Cell.new("B4")
@@ -6,26 +7,26 @@ RSpec.describe Cell do
     expect(cell).to be_instance_of(Cell)
   end
 
-  xit 'tracks a coordinate' do
+  it 'tracks a coordinate' do
     cell = Cell.new("B4")
 
     expect(cell.coordinate).to eq("B4")
   end
 
-  xit 'has no ship' do
+  it 'has no ship' do
     cell = Cell.new("B4")
 
     expect(cell.ship).to eq(nil)
   end
 
-  xit 'is empty' do
+  it 'is empty' do
     cell = Cell.new("B4")
 
 
     expect(cell.empty?).to eq(true)
   end
 
-  xit 'can hold a ship' do
+  it 'can hold a ship' do
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
@@ -33,7 +34,7 @@ RSpec.describe Cell do
     expect(cell.ship).to be_instance_of(Ship)
   end
 
-  xit 'isnt empty' do
+  it 'isnt empty' do
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
@@ -56,20 +57,15 @@ RSpec.describe Cell do
     cell.fire_upon
 
     expect(cell.ship.health).to eq(2)
-
-    it 'knows it was fired upon' do
-      cell = Cell.new("B4")
-      cruiser = Ship.new("Cruiser", 3)
-      cell.place_ship(cruiser)
-      cell.fire_upon
-
-      expect(cell.fired_upon?).to eq(true)
-    end
-
-
   end
 
+  it 'knows it was fired upon' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    cell.fire_upon
 
-
+    expect(cell.fired_upon?).to eq(true)
+  end
 
 end
