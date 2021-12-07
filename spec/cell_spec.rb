e0ea77d957a731da328fb34cb96c4338e7baf82d
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-06 21:04:49 -0500

    feat: add fire upon testing

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -41,13 +41,35 @@ RSpec.describe Cell do
     expect(cell.empty?).to eq(false)
   end
 
-  it 'can hold a ship' do
+  it 'hasnt been fired upon' do
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
 
-    expect(cell.ship).to be_instance_of(Ship)
+    expect(cell.fired_upon?).to eq(false)
   end
+
+  it 'has been fired upon' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    cell.fire_upon
+
+    expect(cell.ship.health).to eq(2)
+
+    it 'knows it was fired upon' do
+      cell = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+      cell.fire_upon
+
+      expect(cell.fired_upon?).to eq(true)
+    end
+
+
+  end
+
+
 
 
 end
