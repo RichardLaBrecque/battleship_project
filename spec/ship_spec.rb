8fe654003e10931306dc9ba298b5e8c0711716da
Tyler Ross <tross020890@gmail.com>
2021-12-06 19:28:27 -0600

    Test: pass ship initialize

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -10,23 +10,23 @@ RSpec.describe Ship do
   end
 
   it 'has a length' do
-    cruiser = Ship.new("Cruiser", 3)
+    ship = Ship.new("Cruiser", 3)
 
 
-    expect(cruiser.length).to eq(3)
+    expect(ship.length).to eq(3)
   end
 
   it 'has a type' do
-    cruiser = Ship.new("Cruiser", 3)
+    ship = Ship.new("Cruiser", 3)
 
 
-    expect(cruiser.name).to eq("Cruiser")
+    expect(ship.name).to eq("Cruiser")
   end
 
   it 'has health' do
-    cruiser = Ship.new("Cruiser", 3)
+    ship = Ship.new("Cruiser", 3)
 
 
-    expect(cruiser.health).to eq(3)
+    expect(ship.health).to eq(3)
   end
 end
