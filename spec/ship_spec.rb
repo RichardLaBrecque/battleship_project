e75553337fd506fa157f0a6957a0ac792dd9f41d
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-06 20:18:48 -0500

    feat: add name, length, and health tests

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,10 +1,32 @@
 require 'pry'
+require './lib/ship'
 
 RSpec.describe Ship do
   it 'exists' do
-    ship = ship.new("Cruiser", 3)
+    ship = Ship.new("Cruiser", 3)
 
 
     expect(ship).to be_instance_of(Ship)
+  end
+
+  it 'has a length' do
+    cruiser = Ship.new("Cruiser", 3)
+
+
+    expect(cruiser.length).to eq(3)
+  end
+
+  it 'has a type' do
+    cruiser = Ship.new("Cruiser", 3)
+
+
+    expect(cruiser.name).to eq("Cruiser")
+  end
+
+  it 'has health' do
+    cruiser = Ship.new("Cruiser", 3)
+
+
+    expect(cruiser.health).to eq(3)
   end
 end
