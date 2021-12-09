9d968683c6828a45f0636f9b3770a993be4402ff
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-09 16:03:13 -0500

    feat: board holds hash of 16 cells

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,7 @@
 require './lib/board'
 require './lib/ship'
 require './lib/cell'
+require 'pry'
 
 RSpec.describe Board do
   it 'exists' do
@@ -10,10 +11,21 @@ RSpec.describe Board do
     expect(board).to be_instance_of(Board)
   end
 
-  it 'has cells' do
+  it 'has a hash of cells' do
+    board = Board.new
+#binding.pry
+    expect(board.cells.class).to eq(Hash)
+  end
+
+  it 'has 16 cells' do
     board = Board.new
 
-    expect(board.cells.class).to be_instance_of(Hash)
     expect(board.cells.keys.size).to eq(16)
+  end
+
+  it 'has cells as values of hash' do
+    board = Board.new
+
+    expect(board.cells.values.first).to be_instance_of(Cell)
   end
 end
