51ec5d219c216fb235a18e9efa72ac8e705a9694
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-09 20:23:07 -0500

    feat:add overlap test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -76,4 +76,14 @@ RSpec.describe Board do
     expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
     expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
   end
+
+  it 'shows false for overlapped ship placement' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq(false)
+
+  end
 end
