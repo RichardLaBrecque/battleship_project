ae828d6f957d2e64bfc3ec5272c8882df63cef65
Tyler Ross <tross020890@gmail.com>
2021-12-10 00:22:26 -0600

    Feat: add valid coordinate

--- lib/board.rb
+++ lib/board.rb
@@ -6,15 +6,22 @@ class Board
   def initialize
     @cells = {}
     cells_array = []
-    coords = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4","D1", "D2", "D3", "D4"]
-    coords.each do |cord|
+    @coords = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4","D1", "D2", "D3", "D4"]
+    @coords.each do |cord|
       cells_array << Cell.new(cord)
     end
     cells_array.each do |cell|
       @cells[cell.coordinate] = cell
     end
   end
+
+  def valid_coordinate?(coord)
+    @coords.include?(coord)
+  end
+
 end
+
+
 #this was thought #1 dont think itll do what i want
 #until @cells.keys.length = 16 do
 #  @cells = Hash.new { |hash, key| hash[cell.coordinate] = cell }
