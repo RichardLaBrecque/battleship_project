6a4f938cde38e11b7e45dcd8e9f17e61c7f05322
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-09 20:03:17 -0500

    feat: add valid placement testing

--- lib/cell_generator.rb
+++ lib/cell_generator.rb
@@ -1,7 +1,7 @@
 require './lib/cell'
 @cells = {}
 cells_array = []
-coords = ["A1", "A2", "A3", "A4"]
+coords = ["A1", "A2", "A3", "A4", "B1", "B2"]
 coords.each do |cord|
   cells_array << Cell.new(cord)
 end
